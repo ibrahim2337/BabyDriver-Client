7da16b46f95b29abdcb5eabd25713a67554cbc62
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-18 16:47:45 +0600

    Footer ready

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,19 +1,30 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
 const Gallery = () => {
-    return (
-        <div className="py-6 dark:bg-gray-800">
-	<div className="container flex flex-col justify-center p-4 mx-auto">
-		<div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
-			<img className="object-cover w-full dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/300x300/?1" />
-			<img className="object-cover w-full dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/300x300/?2" />
-			<img className="object-cover w-full dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/300x300/?3" />
-			<img className="object-cover w-full dark:bg-gray-500 aspect-square" src="https://source.unsplash.com/random/300x300/?4" />
-		</div>
-	</div>
-</div>
-    );
+  return (
+    <div className="container px-6 py-10 mx-auto">
+      <h1 className="text-2xl font-semibold text-center  capitalize lg:text-3xl ">
+        Our Car Toys Gallery
+      </h1>
+      <div className="container flex flex-col justify-center p-4 mx-auto ">
+        <div className="grid grid-cols-1 gap-6 gap-x-20 lg:grid-cols-3 sm:grid-cols-1">
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?1"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?2"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?3"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import Banner from '../../components/Banner/Banner';
 import Footer from '../../components/Footer/Footer';
 import Category from '../../components/Category/Category';
+import Gallery from '../../components/Gallery/Gallery';
 
 const HomePage = () => {
     return (
@@ -11,6 +12,7 @@ const HomePage = () => {
             <Navbar />
             <Banner />
             <Category />
+            <Gallery />
             <Footer />
         </div>
     );
