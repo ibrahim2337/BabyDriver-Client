3a6b8bab191f255c7ea0fadd86f3d97a551af4d8
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-18 17:03:58 +0600

    Gallery section ready

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const Gallery = () => {
   return (
-    <div className="container px-6 py-10 mx-auto">
-      <h1 className="text-2xl font-semibold text-center  capitalize lg:text-3xl ">
+    <div className="container px-10 py-10 mx-auto">
+      <h1 className="text-2xl font-bold text-center mb-16 capitalize lg:text-3xl ">
         Our Car Toys Gallery
       </h1>
-      <div className="container flex flex-col justify-center p-4 mx-auto ">
-        <div className="grid grid-cols-1 gap-6 gap-x-20 lg:grid-cols-3 sm:grid-cols-1">
+      <div>
+        
+        <div className="grid grid-cols-1 gap-6  lg:grid-cols-5 sm:grid-cols-1">
           <img
             className="object-cover w-full  rounded-xl"
             src="https://source.unsplash.com/random/300x300/?1"
@@ -21,7 +22,39 @@ const Gallery = () => {
             className="object-cover w-full  rounded-xl"
             src="https://source.unsplash.com/random/300x300/?3"
           />
+           <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?2"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?3"
+          />
         </div>
+
+        <div className="grid grid-cols-1 gap-6 mt-6 lg:grid-cols-5 sm:grid-cols-1">
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?1"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?2"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?3"
+          />
+           <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?2"
+          />
+          <img
+            className="object-cover w-full  rounded-xl"
+            src="https://source.unsplash.com/random/300x300/?3"
+          />
+        </div>
+
       </div>
     </div>
   );
