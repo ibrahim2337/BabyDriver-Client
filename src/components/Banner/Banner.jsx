13c8496f9e930f4d677bd39218810724bb4ff768
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-18 20:02:36 +0600

    route create and connect successfully

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
@@ -14,15 +15,15 @@ const Banner = () => {
                 <span className="text-blue-500 ">Toys</span>
               </h1>
 
-              <p className="mt-3 text-gray-600 ">
+              <p className="mt-3 text-gray-600 mb-5 ">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
                 beatae error laborum ab amet sunt recusandae? Reiciendis natus
                 perspiciatis optio.
               </p>
 
-              <button className="px-6 py-2 mt-6 text-sm font-semibold rounded-md text-white bg-gradient-to-r from-gray-600 to-gray-400">
+              <Link to="/products" className="px-6 py-2 text-sm font-semibold rounded-md text-white bg-gradient-to-r from-gray-600 to-gray-400">
                 Shop Now
-              </button>
+              </Link>
             </div>
           </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { RouterProvider, createBrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import ErrorPage from './pages/ErrorPage/ErrorPage';
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ProductsPage from './pages/ProductsPage/ProductsPage';
-import Blogs from './components/Blogs/Blogs';
-import Contact from './components/Contact/Contact';
-
-
-
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import Blogs from "./components/Blogs/Blogs";
+import Contact from "./components/Contact/Contact";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App />,
-    errorElement:<ErrorPage />,
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path:"/",
-        element:<HomePage />,
+        path: "/",
+        element: <HomePage />,
       },
       {
-        path:"/home",
-        element:<HomePage />,
+        path: "/home",
+        element: <HomePage />,
       },
       {
-        path:"/products",
-        element:<ProductsPage />,
+        path: "/products",
+        element: <ProductsPage />,
       },
       {
-        path:"/contact",
-        element:<Contact />,
+        path: "/contact",
+        element: <Contact />,
       },
       {
-        path:"/blogs",
-        element:<Blogs />,
+        path: "/blogs",
+        element: <Blogs />,
       },
       {
-        path:"/login",
-        element:<LoginPage />,
+        path: "/login",
+        element: <LoginPage />,
       },
-    ]
-  }
+    ],
+  },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
- <div>
-  
-  <RouterProvider router={router} />
-  
- </div>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <RouterProvider router={router} />
+  </div>
+);
